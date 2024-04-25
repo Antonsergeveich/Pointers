1b7880e0fedd186fe547f2155bbd1d45f270536c
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-04-25 13:43:48 +0300

    Transfer Parameters

--- TransferParameters/main.cpp
+++ TransferParameters/main.cpp
@@ -1,14 +1,22 @@
 #include<iostream>
 using namespace std;
 
-void Exchange(int& a, int& b);
+/*
+1. By value;
+2. By Pointer;
+3. By reference;
+*/
+
+void Exchange(int& a, int& b);// по ссылке
+void Exchange_1(int* a, int* b);// по указателю
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	int a = 2, b = 3;
 	cout << a << "\t" << b << endl;
-	Exchange(a, b);
+	Exchange(a, b);//по ссылке
+	//Exchange_1(&a, &b);// по указателю
 	cout << a << "\t" << b << endl;
 }
 
@@ -17,4 +25,12 @@ void Exchange(int& a, int& b)
 	int buffer = a;
 	a = b;
 	b = buffer;
+}
+
+void Exchange_1(int* a, int* b)
+{
+	
+	int buffer = *a;
+	*a = *b;
+	* b = buffer;
 }
