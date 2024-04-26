4cbcd5e3518219520b3f3db08a6045cbd8bfb77e
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-04-26 20:50:08 +0300

    All Done

---
+++ Split/main.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+#define tab "\t"
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n]{};
+	FillRand(arr, n);
+	Print(arr, n);
+
+	//1)Вычисляем количество чётных и не чётных элементов:
+	int even_count = 0;
+	int odd_count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)even_count++;
+		else odd_count++;
+	}
+	cout << "Количество чётных элементов: " << even_count << endl;
+	cout << "Количество  не чётных элементов: " << odd_count << endl;
+
+	//2) Выделяем память для массивов:
+	int* even_arr = new int[even_count];
+	int* odd_arr = new int[odd_count];
+
+	//3)Копируем чётные и не чётные элементы в соответствующие массивы:
+	for (int i = 0, j = 0, k = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			even_arr[j] = arr[i];
+			j++;
+		}
+		else
+		{
+			odd_arr[k] = arr[i];
+			k++;
+		}
+	}
+	//4)Вывод результатов:
+	Print(even_arr, even_count);
+	Print(odd_arr, odd_count);
+
+	//5)Удаление массивов:
+	delete[] odd_arr;
+	delete[] even_arr;
+
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
