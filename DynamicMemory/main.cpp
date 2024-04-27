3ad81f0984ba42ffa4de0e8b0705da29e3a43fe0
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-04-27 12:41:01 +0300

    all done

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -1,18 +1,41 @@
 #include<iostream>
 using namespace std;
+
 #define tab "\t"
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	int n;
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n];
-	for (int i = 0; i < n; i++)
-	{
+	
+	FillRand(arr, n);
+	Print(arr, n);
+	int value;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr[n] = value;
+	n++;
+	Print(arr, n);
+
+	delete[] arr;
+}
+
+void FillRand(int arr[], const int n)
+{
+	     for (int i = 0; i < n; i++)
+	     {
 		//Обращение к элементам массива через арифметику указателей
 		//и оператор разъименования
-		*(arr + i) = rand() % 100;
-	}
+		      *(arr + i) = rand() % 100;
+	     }
+}
+
+void Print(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		//обращение к элементам массива через 
@@ -20,6 +43,4 @@ void main()
 		cout << arr[i] << tab;
 	}
 	cout << endl;
-
-	delete[] arr;
 }
--- Split/main.cpp
+++ Split/main.cpp
@@ -19,7 +19,8 @@ void main()
 	int odd_count = 0;
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] % 2 == 0 ? even_count++ : odd_count++;
+		(arr[i] & 1) == 0 ? even_count++ : odd_count++;
+		//arr[i] % 2 == 0 ? even_count++ : odd_count++;
 		/*if (arr[i] % 2 == 0)even_count++;
 		else odd_count++;*/
 	}
