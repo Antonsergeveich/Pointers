bf2bf383bf29fa4e674ad3a71fc27b84b2520aed
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-05-15 19:44:34 +0300

    all done

--- DynamicMemory/Allocate.cpp
+++ DynamicMemory/Allocate.cpp
@@ -2,7 +2,7 @@
 T** Allocate(const int rows, const int cols)
 {
 	//1) Создаём массив указателей :
-	int** arr = new int* [rows];
+	T** arr = new int* [rows];
 
 	//2) Выделяем память под строки:
 	for (int i = 0; i < rows; i++)
