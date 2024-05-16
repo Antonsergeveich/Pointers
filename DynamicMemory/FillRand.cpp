cd8e3b7512e0fb572214d6307e3df1a7821f32b7
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-05-17 00:45:49 +0300

    2

--- DynamicMemory/FillRand.cpp
+++ DynamicMemory/FillRand.cpp
@@ -1,39 +1,5 @@
 #include"stdafx.h"
-void FillRand(int arr[], const int n, int minRand, int maxRand)
-{
-	for (int i = 0; i < n; i++)
-	{
-		//Обращение к элементам массива через арифметику указателей
-		//и оператор разъименования
-		*(arr + i) = rand() % (maxRand - minRand) + minRand;
-	}
-}
-void FillRand(double arr[], const int n, int minRand, int maxRand)
-{
-	minRand *= 100;
-	maxRand *= 100;
-	for (int i = 0; i < n; i++)
-	{
-		//Обращение к элементам массива через арифметику указателей
-		//и оператор разъименования
-		*(arr + i) = rand() % (maxRand - minRand) + minRand;
-		*(arr + i) /= 100;
-	}
-}
-void FillRand(char arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		*(arr + i) = rand();
-	}
-}
-void FillRand(int** arr, const int rows, const int cols)
-{
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < cols; j++)
-		{
-			arr[i][j] = rand() % 100;
-		}
-	}
-}
+void FillRand(int arr[],const int n,int minRand,int maxRand){for(int i=0;i<n;i++) *(arr+i)=rand()%(maxRand-minRand)+minRand;}
+void FillRand(double arr[],const int n,int minRand,int maxRand){minRand*=100;maxRand*=100;for(int i=0; i<n;i++){*(arr+i)=rand()%(maxRand-minRand)+minRand;*(arr+i)/=100;}}
+void FillRand(int**arr,const int rows,const int cols){for(int i=0;i<rows;i++){for(int j=0;j<cols;j++)arr[i][j]=rand()%100;}}
+void FillRand(char arr[],const int n){for(int i=0;i<n;i++) *(arr+i)=rand();}
--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -51,12 +51,7 @@ void main()
 	delete[] arr;
 #endif // DYNAMIC_MEMORY_1
 #ifdef DYNAMIC_MEMORY_2
-	int rows;
-	int cols;
-	int index;
-	cout << "Введите количество строк: "; cin >> rows;
-	cout << "Введите количество элементов строки: "; cin >> cols;
-	
+	int rows,cols,index;cout<<"Введите строки, столбцы, индекс: ";cin>>rows;cin>>cols;cin>>index;
 	int** arr = Allocate(rows, cols);
 	FillRand(arr, rows, cols);
 	Print(arr, rows, cols);
@@ -68,13 +63,11 @@ void main()
 	arr = push_row_back(arr, rows, cols);
 	FillRand(arr[rows - 1], cols, 100, 1000);
 	Print(arr, rows, cols);
-
-	cout << "Введите индекс вставляемой строки: "; cin >> index;
+		
 	arr = insert_row(arr, rows, cols, index);
 	FillRand(arr[index], cols);
 	Print(arr, rows, cols);
-
-	cout << "Введите индекс удаляемой строки: "; cin >> index;
+		
 	arr = erase_row(arr, rows, index);
 	Print(arr, rows, cols);
 
@@ -90,7 +83,7 @@ void main()
 	push_col_front(arr, rows, cols);
 	Print(arr, rows, cols);
 
-	cout << "Введите индекс вставляемого столбца: "; cin >> index;
+	
 	insert_col(arr, rows, cols, index);
 	Print(arr, rows, cols);
 
@@ -100,26 +93,13 @@ void main()
 	pop_col_front(arr, rows, cols);
 	Print(arr, rows, cols);
 
-	cout << "Введите индекс удаляемого столбца: "; cin >> index;
 	erase_col(arr, rows, cols, index);
 	Print(arr, rows, cols);
 
 	Clear(arr, rows);
 #endif // DYNAMIC_MEMORY_2
 }
-//Stack - это модель памяти, из которой последний записанный элемент считывается первым.
-//push - вставить //pop - вытащить
 
-int** Allocate(const int rows, const int cols)
-{
-	//1) Создаём массив указателей :
-	int** arr = new int* [rows];
-
-	//2) Выделяем память под строки:
-	for (int i = 0; i < rows; i++)
-	{
-		arr[i] = new int[cols] {};
-	}
-	return arr;
-}
+int** Allocate(const int rows, const int cols){int** arr = new int* [rows];
+	for (int i = 0; i < rows; i++)arr[i] = new int[cols] {}; return arr;}
 
