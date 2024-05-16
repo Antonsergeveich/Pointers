8b825d7fedb20c875fc5531d0b218a2a73c1d4fe
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-05-17 01:59:37 +0300

    3

--- DynamicMemory/Pop.h
+++ DynamicMemory/Pop.h
@@ -1,27 +1,11 @@
 #pragma once
-template<typename T>
-T* pop_back(T arr[], int& n);
-
-template<typename T>
-T* pop_front(T arr[], int& n);
-
-template<typename T>
-T* erase(T arr[], int& n, int& index);
-
-template<typename T>
-T** pop_row_back(T** arr, int& rows);
-
-template<typename T>
-T** pop_row_front(T** arr, int& rows);
-
-template<typename T>
-T** erase_row(T** arr, int& rows, int& index);
-
-template<typename T>
-void pop_col_back(T** arr, const int rows, int& cols);
-
-template<typename T>
-void pop_col_front(T** arr, const int rows, int& cols);
-
-template<typename T>
-void erase_col(T** arr, const int rows, int& cols, const int index);
+#define TT template<typename T>
+TT T* pop_back(T arr[],int&n);
+TT T* pop_front(T arr[],int&n);
+TT T* erase(T arr[],int&n,int&index);
+TT T** pop_row_back(T**arr,int&rows);
+TT T** pop_row_front(T**arr,int&rows);
+TT T** erase_row(T**arr,int&rows,int&index);
+TT void pop_col_back(T**arr, const int rows,int&cols);
+TT void pop_col_front(T**arr,const int rows,int&cols);
+TT void erase_col(T**arr,const int rows,int&cols,const int index);
